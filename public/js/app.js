494a113c23b56899688d26dbcb5eb48642111fee
jeastburn <jim.eastburn@balfour.com>
2013-11-27 16:55:58 -0600

    addText and addCircle functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+
+
 window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
 
 angular.module('mean.system', []);
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,22 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+
     $scope.global = Global;
+
+    $scope.addTextBox = function(){
+        var s = Snap("#svg");
+        s.text(200, 100, "Text box...").drag();
+    };
+
+    $scope.addCircle = function(){
+        var s = Snap("#svg");
+        var bigCircle = s.circle(150, 150, 100).drag();
+
+        bigCircle.attr({
+            fill: "#bada55",
+            stroke: "#000",
+            strokeWidth: 5
+        });
+    };
+
+
 }]);

961bb30fa197d4849a2364a89f91e4981cfb2ac6
jeastburn <jim.eastburn@balfour.com>
2013-12-06 09:53:23 -0600

    now able to download Hello World pdf using jspdf.js in the browser.

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -7,6 +7,20 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
         s.text(200, 100, "Text box...").drag();
     };
 
+    $scope.outputPdf = function(){
+        var doc = new jsPDF();
+        doc.text(20, 20, 'Hello world.');
+        doc.save('Test.pdf');
+
+//        var doc = new jsPDF();
+//        var s = Snap("#svg");
+//
+//        var svgPdf = new svgElementToPdf(s, doc, options)
+//
+//        svgPdf.save('Test.pdf');
+
+    };
+
     $scope.addCircle = function(){
         var s = Snap("#svg");
         var bigCircle = s.circle(150, 150, 100).drag();

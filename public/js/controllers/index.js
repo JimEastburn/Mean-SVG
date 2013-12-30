57fa8986dcfbafd08c5a0f53b5c0da74ccb3b5ab
jeastburn <jim.eastburn@balfour.com>
2013-12-30 09:56:24 -0600

    animate right handle drag on square

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -12,14 +12,27 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
 //        doc.text(20, 20, 'Hello world.');
 //        doc.save('Test.pdf');
 
-        var doc = new jsPDF();
+
+       // https://github.com/CBiX/svgToPdf.js
+//        // I recommend to keep the svg visible as a preview
+//        var svg = $('#container > svg').get(0);
+//// you should set the format dynamically, write [width, height] instead of 'a4'
+//        var pdf = new jsPDF('p', 'pt', 'a4');
+//        svgElementToPdf(svg, pdf, {
+//            scale: 72/96, // this is the ratio of px to pt units
+//            removeInvalid: true // this removes elements that could not be translated to pdf from the source svg
+//        });
+//        pdf.output('datauri'); // use output() to get the jsPDF buffer
+
+        var pdfdoc = new jsPDF();
         var s = $("#svg");
         var options = {};
         options.removeInvalid = false;
+        options.scale = 1.0;
         options.x_offset = 0;
         options.y_offset = 0;
         options.ego_or_link = false;
-        var svgPdf = doc.svgElementToPdf(s, doc, options);
+        var svgPdf = pdfdoc.svgElementToPdf(s, pdfdoc, options);
 
         svgPdf.save('Test.pdf');
 
@@ -34,6 +47,83 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
             stroke: "#000",
             strokeWidth: 5
         });
+    };
+
+    $scope.addSquare = function(){
+        var s = Snap("#svg");
+        var bigSquare = s.rect(100, 100, 200, 200);
+//        var topLeft = s.rect(90, 90, 10, 10).drag(topLeftOnMove(),topLeftOnStart(),topLeftOnEnd());
+//        var topRight = s.rect(300, 90, 10, 10).drag(topRightOnMove(),topRightOnStart(),topRightOnEnd());
+//        var bottomRight = s.rect(300, 300, 10, 10).drag(bottomRightOnMove(),bottomRightOnStart(),bottomRightOnEnd());
+//        var bottomLeft = s.rect(90, 300, 10, 10).drag(bottomLeftOnMove(),bottomLeftOnStart(),bottomLeftOnEnd());
+
+        var topLeft = s.rect(90, 90, 10, 10).drag();
+        var topRight = s.rect(300, 90, 10, 10).drag();
+
+        var bottomLeft = s.rect(90, 300, 10, 10).drag();
+
+        //var dropTarget = s.group(bigSquare, topLeft, topRight, bottomRight, bottomLeft);
+
+        bigSquare.attr({
+            fill: "#fff",
+            stroke: "#000",
+            strokeWidth: 5
+        });
+
+        var bottomRightOnMove = function(dx, dy, x, y, e){
+            var a = dx;
+            var b = dy;
+            var c = x;
+            var d = y;
+            var evt = e;
+            var bigSquareWidth = bigSquare.width;
+            bigSquare.animate({width: 400, height:400}, 1);
+
+
+        };
+        var bottomRightOnStart = function(x, y, e){
+            var c = x;
+            var d = y;
+            var evt = e;
+
+        };
+        var bottomRightOnEnd = function(e){
+
+            var evt = e;
+
+        };
+
+        var bottomRight = s.rect(300, 300, 10, 10).drag(bottomRightOnMove,bottomRightOnStart,bottomRightOnEnd);
+//        var topLeftOnMove = function(){
+//
+//        };
+//        var topLeftOnStart = function(){
+//
+//        };
+//        var topLeftOnEnd = function(){
+//
+//        };
+//        var topRightOnMove = function(){
+//
+//        };
+//        var topRightOnStart = function(){
+//
+//        };
+//        var topRightOnEnd = function(){
+//
+//        };
+//
+//        var bottomLeftOnMove = function(){
+//
+//        };
+//        var bottomLeftOnStart = function(){
+//
+//        };
+//        var bottomLeftOnEnd = function(){
+//
+//        };
+
+
     };
 
 

086b9e74ad7a703e3519c585b93f72374446c4e9
jeastburn <jim.eastburn@balfour.com>
2014-02-11 08:14:40 -0600

    resize - new implementation - using circles now as resize handles - sw is the only handle not working

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -57,10 +57,15 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
 
         var image = s.image("/../img/Chrysanthemum.jpg", 100, 100, 200, 200);
 
-        var topLeft = s.rect(90, 90, 10, 10);
-        var topRight = s.rect(300, 90, 10, 10);
-        var bottomRight = s.rect(300, 300, 10, 10);
-        var bottomLeft = s.rect(90, 300, 10, 10);
+//        var topLeft = s.rect(90, 90, 10, 10);
+//        var topRight = s.rect(300, 90, 10, 10);
+//        var bottomRight = s.rect(300, 300, 10, 10);
+//        var bottomLeft = s.rect(90, 300, 10, 10);
+
+        var topLeft = s.circle(100, 100, 7);
+        var topRight = s.circle(300, 100, 7);
+        var bottomRight = s.circle(300, 300, 7);
+        var bottomLeft = s.circle(100, 300, 7);
 
         bigSquare.attr({
             fill: "#fff",
@@ -84,15 +89,15 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
             // Inspect cursor to determine which resize/move process to use
             switch (this.attr('cursor')) {
 
-                case 'nw-resize' :    //bigSquare-0, image-1, topLeft-2, topRight-3, bottomRight-4, bottomLeft-5
-                    this[0].attr({          //working
+                case 'nw-resize' :
+                    this[0].attr({
                         x:  e.offsetX,
                         y:  e.offsetY,
                         width: this.ow - dx,
                         height: this.oh - dy
                     });
 
-                    this[1].attr({          //working
+                    this[1].attr({
                         x:  e.offsetX,
                         y:  e.offsetY,
                         width: this.ow - dx,
@@ -100,24 +105,24 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     });
 
                     this[2].attr({//topLeft nw   working
-                        x:  e.offsetX -10,
-                        y:  e.offsetY - 10
+                        cx:  e.offsetX,
+                        cy:  e.offsetY
                     });
 
                     this[3].attr({//topRight  ne  working
-                        x:  e.offsetX + (this.ow - dx),
-                        y:  e.offsetY - 10
+                        cx:  e.offsetX + (this.ow - dx),
+                        cy:  e.offsetY
 
                     });
 
                     this[4].attr({//bottomRight   se  working
-                        x:  e.offsetX + (this.ow - dx),
-                        y:  e.offsetY + (this.oh - dy)
+                        cx:  e.offsetX + (this.ow - dx),
+                        cy:  e.offsetY + (this.oh - dy)
                     });
 
                     this[5].attr({//bottomLeft  sw   working
-                        x:  e.offsetX - 10 ,
-                        y:  e.offsetY + (this.oh - dy)
+                        cx:  e.offsetX  ,
+                        cy:  e.offsetY + (this.oh - dy)
                     });
                     break;
 
@@ -135,24 +140,24 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     });
 
                     this[2].attr({//topLeft nw
-                        x:  e.offsetX - this.ow - dx ,
-                        y:  e.offsetY - 10
+                        cx:  e.offsetX - this.ow - dx ,
+                        cy:  e.offsetY
                     });
 
                     this[3].attr({//topRight  ne  works
-                        x:  e.offsetX ,
-                        y:  e.offsetY - 10
+                        cx:  e.offsetX ,
+                        cy:  e.offsetY
 
                     });
 
                     this[4].attr({//bottomRight   se  works
-                        x:  e.offsetX,
-                        y:  (e.offsetY) + (this.oh - dy)
+                        cx:  e.offsetX,
+                        cy:  (e.offsetY) + (this.oh - dy)
                     });
 
                     this[5].attr({//bottomLeft  sw
-                        x:  e.offsetX - this.ow - dx,
-                        y:  (e.offsetY) + (this.oh - dy)
+                        cx:  e.offsetX - this.ow - dx,
+                        cy:  (e.offsetY) + (this.oh - dy)
                     });
                     break;
 
@@ -172,16 +177,16 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     });
 
                     this[3].attr({//topRight works
-                        x:  e.offsetX
+                        cx:  e.offsetX
                     });
 
                     this[4].attr({//bottomRight  works
-                        x:  e.offsetX ,
-                        y:  e.offsetY
+                        cx:  e.offsetX ,
+                        cy:  e.offsetY
                     });
 
                     this[5].attr({//bottomLeft  works
-                        y:  e.offsetY
+                        cy:  e.offsetY
                     });
 
 
@@ -199,6 +204,24 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                         width: this.ow - dx,
                         height: e.offsetY - this.oy
                     });
+
+                    this[2].attr({//topLeft works
+                        //no changes
+                    });
+
+                    this[3].attr({//topRight works
+//                        cx:  e.offsetX
+                    });
+
+                    this[4].attr({//bottomRight  works
+
+                        cy:  e.offsetY + (this.oy )
+                    });
+
+                    this[5].attr({//bottomLeft  works
+                        cx:  e.offsetX ,
+                        cy:  e.offsetY
+                    });
                     break;
 
                 default :             //bigSquare-0, image-1, topLeft-2, topRight-3, bottomRight-4, bottomLeft-5
@@ -213,23 +236,23 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     });
 
                     this[2].attr({//topLeft
-                        x:  e.offsetX - (this.ow *.5)-10 ,
-                        y:  e.offsetY - (this.oh *.5)-10
+                        cx:  e.offsetX - (this.ow *.5) ,
+                        cy:  e.offsetY - (this.oh *.5)
                     });
 
                     this[3].attr({//topRight
-                        x:  e.offsetX +(this.ow *.5) ,
-                        y:  e.offsetY - (this.oh *.5)-10
+                        cx:  e.offsetX +(this.ow *.5) ,
+                        cy:  e.offsetY - (this.oh *.5)
                     });
 
                     this[4].attr({//bottomRight
-                        x:  e.offsetX +(this.ow *.5) ,
-                        y:  e.offsetY + (this.oh *.5)
+                        cx:  e.offsetX +(this.ow *.5) ,
+                        cy:  e.offsetY + (this.oh *.5)
                     });
 
                     this[5].attr({//bottomLeft
-                        x:  e.offsetX -(this.ow *.5)-10 ,
-                        y:  e.offsetY + (this.oh *.5)
+                        cx:  e.offsetX -(this.ow *.5) ,
+                        cy:  e.offsetY + (this.oh *.5)
                     });
                     break;
 
